778d922d56a3b68bf1920308b8fc9cdc566200c0
Facundo Ebbio <facuebbio@gmail.com>
2023-10-18 10:32:07 +0200

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css'
+import App from './App.jsx'
+
+
+
+
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
+
